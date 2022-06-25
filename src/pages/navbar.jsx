5f793e3966938eeea18d2c00fb28b23ce3211dfc
RahulRST <srahuljayaraj@gmail.com>
2022-06-25 21:16:19 +0530

    Changes

--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -4,12 +4,12 @@ function Navbar() {
     return(
     <nav>
       <ul>
-        <li><a href="#">Home</a></li>
+        <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
     </nav>   
-    )
+    );
 }
 
 export default Navbar;
